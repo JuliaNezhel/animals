272cdc28faabd73385b50505d59695b87ee7279e
JuliaNezhel <yulyazelikova@gmail.com>
2024-04-05 13:03:12 +0300

    добавление верстки

---
+++ animals/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/layout/header/Header";
+import { Section } from "./components/layout/section/Section";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
